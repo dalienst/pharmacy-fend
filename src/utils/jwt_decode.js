8689ab1188644130041f918999734c5ea1b90114
dalienst <oduordalienst@gmail.com>
2022-09-06 10:51:52 +0300

    fix persistant login

--- src/utils/jwt_decode.js
+++ src/utils/jwt_decode.js
@@ -1,8 +1,12 @@
+// eslint-disable-next-line camelcase
 import jwt_decode from 'jwt-decode';
 
 const jwtDecode = (token) => {
-  const decoded = jwt_decode(token);
-  return decoded.user_id;
+  if (token) {
+    const decoded = jwt_decode(token);
+    return decoded.user_id;
+  }
+  return undefined;
 };
 
 export default jwtDecode;
