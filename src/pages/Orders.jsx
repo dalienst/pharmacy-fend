2949fb3e40ee9bea545e1ff477775a70b6fc758d
dalienst <oduordalienst@gmail.com>
2022-09-15 00:20:14 +0300

    remove console log

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -28,7 +28,6 @@ export default function Orders() {
       try {
         const response = await axiosPrivate.get(urls.ORDERS);
         setOrders(response.data);
-        console.log(response.data);
       } catch (error) {
         // toast.error('Cannot fetch distributors at this time')
       }
